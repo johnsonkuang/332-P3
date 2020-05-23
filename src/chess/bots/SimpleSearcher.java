b23680bcc6d82511b4f6c5e894b83052b69df860
albert-zhong <albertzhong@hotmail.com>
2020-05-23 11:50:01 -0700

    Add SimpleSearcher.java

--- src/chess/bots/SimpleSearcher.java
+++ src/chess/bots/SimpleSearcher.java
@@ -4,7 +4,8 @@ import cse332.chess.interfaces.AbstractSearcher;
 import cse332.chess.interfaces.Board;
 import cse332.chess.interfaces.Evaluator;
 import cse332.chess.interfaces.Move;
-import cse332.exceptions.NotYetImplementedException;
+
+import java.util.*;
 
 /**
  * This class should implement the minimax algorithm as described in the
@@ -20,6 +21,36 @@ public class SimpleSearcher<M extends Move<M>, B extends Board<M, B>> extends
     }
 
     static <M extends Move<M>, B extends Board<M, B>> BestMove<M> minimax(Evaluator<B> evaluator, B board, int depth) {
-        throw new NotYetImplementedException();
+        Set<Board> seen = new HashSet<Board>();
+        return minimax(evaluator, board, depth, seen);
     }
+
+    static <M extends Move<M>, B extends Board<M, B>> BestMove<M> minimax (
+            Evaluator<B> evaluator, B board, int depth, Set<Board> seen) {
+        if (depth == 0 || seen.contains(board)) {
+            return new BestMove<M>(evaluator.eval(board));
+        }
+        seen.add(board);
+
+        List<M> moves = board.generateMoves();
+        if (moves.isEmpty()) {
+            return new BestMove<M>(board.inCheck() ? -evaluator.mate() - depth : -evaluator.stalemate());
+        }
+
+        BestMove<M> bestMove = new BestMove<M>(-evaluator.infty());
+
+        for (M move : moves) {
+            board.applyMove(move);
+
+            BestMove<M> currMove = minimax(evaluator, board, depth - 1, cached, seen).negate();
+            board.undoMove();
+            if (currMove.value > bestMove.value) {
+                bestMove = currMove;
+                bestMove.move = move;
+            }
+        }
+
+        return bestMove;
+    }
+
 }
